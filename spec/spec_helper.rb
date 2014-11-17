58d1b0e6940fce04420c3a191072c46b09c4e43f
Pavel Redics <pavel.redics@gmail.com>
2014-11-17 21:20:14 +0000

    added capybara to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 ENV["RACK_ENV"] = 'test' 
 
 require 'bookmark_manager'
+require 'database_cleaner'
+require 'capybara/rspec'
+
+Capybara.app = Sinatra::Application.new
 
 # include {:focus=>true}
 
@@ -21,6 +25,19 @@ require 'bookmark_manager'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
