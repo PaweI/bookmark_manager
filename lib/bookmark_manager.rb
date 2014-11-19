aaa02f6e70c53aa12ba38a0c967327946df1fac9
Pavel Redics <pavel.redics@gmail.com>
2014-11-19 14:03:40 +0000

    test works

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -3,6 +3,7 @@ require 'data_mapper'
 
 env = ENV["RACK_ENV"] || "development"
 
+
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require "./lib/link"
@@ -11,7 +12,11 @@ DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
+
 class BookmarkManager < Sinatra::Base
+
+set :views, Proc.new { File.join(root, '..', 'views') }
+
   get '/' do
     @links = Link.all
     erb :index
--- lib/link.rb
+++ lib/link.rb
@@ -1,7 +1,6 @@
 require 'data_mapper'
 
 class Link
-
   include DataMapper::Resource
 
   property :id,     Serial
