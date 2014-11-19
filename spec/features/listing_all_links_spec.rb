56d9086740fa41e8d2174ed61c5e5b54965dc750
Pavel Redics <pavel.redics@gmail.com>
2014-11-19 19:39:35 +0000

    create separate file for test listing links

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -1,24 +1,14 @@
 require 'spec_helper'
 
-feature "User adds a new link" do
+feature "User browses the list of links" do
 
-  # before(:each) { Link.create(:url => "http://www.makersacademy.com", :title => "Makers Academy") }
+  before(:each) { 
+    Link.create(:url => "http://www.makersacademy.com", 
+                              :title => "Makers Academy") 
+  }
 
   scenario "when browsing the homepage" do
     visit '/'
-    add_link("http://www.makersacademy.com/", "Makers Academy")
-    # expect(page).to have_content("Makers Academy")
-    expect(Link.count).to eq 1
-    link = Link.first
-    expect(link.url).to eq("http://www.makersacademy.com/")
-    expect(link.title).to eq("Makers Academy")
-  end
-
-  def add_link(url, title)
-    within('#new-link') do
-      fill_in 'url', :with => url
-      fill_in 'title', :with => title
-      click_button 'Add link'
-    end
+    expect(page).to have_content("Makers Academy")
   end
 end
