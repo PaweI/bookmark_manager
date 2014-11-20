9b116a024672a5f8e5dd10468536d4a1661ac720
Pavel Redics <pavel.redics@gmail.com>
2014-11-20 15:51:43 +0000

    prevent from creating user if already exist, display errors from user file

--- lib/user.rb
+++ lib/user.rb
@@ -9,10 +9,10 @@ class User
   attr_accessor :password_confirmation
 
   property :id,              Serial
-  property :email,           String, :unique => true
+  property :email,           String, :unique => true, :message => "This email is already taken"
   property :password_digest, Text
 
-  validates_confirmation_of :password
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
 
   def password=(password) 
     @password = password
