541e5c83496e8bb521680ba26ab44c6251ea64bb
Pavel Redics <pavel.redics@gmail.com>
2014-11-20 10:20:48 +0000

    Adding user account + password

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -8,6 +8,7 @@ DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require "./lib/link"
 require "./lib/tag"
+require "./lib/user"
 
 DataMapper.finalize
 
@@ -15,6 +16,10 @@ DataMapper.auto_upgrade!
 
 
 class BookmarkManager < Sinatra::Base
+
+enable :sessions
+
+set :session_secret, 'super secret'
 
 set :views, Proc.new { File.join(root, '..', 'views') }
 
@@ -29,12 +34,31 @@ set :views, Proc.new { File.join(root, '..', 'views') }
     erb :index
   end
 
+  get '/users/new' do
+    erb :"users/new/new"
+  end
+
   post '/links' do
     url = params["url"]
     title = params["title"]
     tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(:text => tag) }
     Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
+  end
+
+  post '/users' do
+    user = User.create(:email => params[:email],
+                       :password => params[:password])
+    session['user_id'] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
+
   end
 
   # start the server if ruby file executed directly
