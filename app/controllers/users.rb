4a455bb0d7119d928bdfae1bb501463e03435fa2
Pavel Redics <pavel.redics@gmail.com>
2014-11-23 23:19:42 +0000

    put mail gun into helpers for now

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -19,7 +19,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/reset_password/:token' do
-    erb :"users/new_password"
+    user = User.first(:password_token => params[:token])
+    if Time.now - user.password_token_timestamp < 86400 # day in seconds
+      erb :"users/new_password"
+    else
+      flash[:notice] = "Sorry, your link to reset password is expired"
+      redirect to('/sessions/new')
+    end
   end
 
   post '/users/forgot_password' do
@@ -33,15 +39,6 @@ class BookmarkManager < Sinatra::Base
       flash[:notice] = "Sorry, invalid user email"
       redirect to('/sessions/new')
     end
-  end
-
-  def send_simple_message
-    RestClient.post "https://api:key-511213ccddec0d0011222357a21c3558"\
-    "@api.mailgun.net/v2/sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org/messages",
-    :from => "Mailgun Sandbox <postmaster@sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org>",
-    :to => "Pavel <pavel.redics@gmail.com>",
-    :subject => "Hello Pavel",
-    :text => "Here is your link to change your password http://localhost:9292/users/reset_password/:#{token}"
   end
 
 end
--- app/helpers/application.rb
+++ app/helpers/application.rb
@@ -10,4 +10,13 @@ def timestamp
   @timestamp = Time.now
 end
 
+def send_simple_message
+  RestClient.post "https://api:key-511213ccddec0d0011222357a21c3558"\
+  "@api.mailgun.net/v2/sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org/messages",
+  :from => "Mailgun Sandbox <postmaster@sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org>",
+  :to => "Pavel <pavel.redics@gmail.com>",
+  :subject => "Hello Pavel",
+  :text => "Here is your link to change your password http://localhost:9292/users/reset_password/:#{token}"
+end
+
 
