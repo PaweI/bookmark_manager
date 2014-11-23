656652245a61dd8a8947b3fbb42458e4e6c0f3ec
Pavel Redics <pavel.redics@gmail.com>
2014-11-23 22:55:29 +0000

    Added mailgun, send email

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -19,13 +19,14 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/reset_password/:token' do
-    erb :"sessions/new"
+    erb :"users/new_password"
   end
 
   post '/users/forgot_password' do
     user = User.first(:email => params[:email])
     if user
       user.update(password_token: token, password_token_timestamp: timestamp)
+      send_simple_message
       flash[:notice] = "Instructions to reset password being sent to #{params[:email]}"
       redirect to('/')
     else
@@ -35,12 +36,12 @@ class BookmarkManager < Sinatra::Base
   end
 
   def send_simple_message
-  RestClient.post "https://api:key-511213ccddec0d0011222357a21c3558"\
-  "@api.mailgun.net/v2/sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org/messages",
-  :from => "Mailgun Sandbox <postmaster@sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org>",
-  :to => "Pavel <pavel.redics@gmail.com>",
-  :subject => "Hello Pavel",
-  :text => "Congratulations Pavel, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."
-end
+    RestClient.post "https://api:key-511213ccddec0d0011222357a21c3558"\
+    "@api.mailgun.net/v2/sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org/messages",
+    :from => "Mailgun Sandbox <postmaster@sandboxade8e1953e7f44919e21034bcd53a3b4.mailgun.org>",
+    :to => "Pavel <pavel.redics@gmail.com>",
+    :subject => "Hello Pavel",
+    :text => "Here is your link to change your password http://localhost:9292/users/reset_password/:#{token}"
+  end
 
 end
