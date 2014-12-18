e279befe9960b49a5d73dd11ea16358700222871
Pavel Redics <pavel.redics@gmail.com>
2014-12-18 17:48:03 +0000

    indenting in bookmark_manager.rb

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -11,20 +11,19 @@ require_relative 'controllers/controllers_loader'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+  set :public_folder, File.join(root, '../..', 'public') 
+  set :views, File.join(root, '..', 'views')
+  use Rack::MethodOverride
+  use Rack::Flash
 
-
-enable :sessions
-set :session_secret, 'super secret'
-set :public_folder, File.join(root, '../..', 'public') 
-set :views, File.join(root, '..', 'views')
-use Rack::MethodOverride
-use Rack::Flash
-
-configure do
-  register Sinatra::Partial
-  set :partial_template_engine, :erb
-end
+  configure do
+    register Sinatra::Partial
+    set :partial_template_engine, :erb
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+    
 end
