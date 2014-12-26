0ca72272095e5614b595e26bd4c109f322bad031
Pavel Redics <pavel.redics@gmail.com>
2014-12-26 22:09:23 +0000

    remove auto_ugrade from data_mapper_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,8 @@
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_BLUE_URL'])
+# ENV['HEROKU_POSTGRESQL_BLUE_URL'] Database when deploying to Heroku
+
+DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
 require_relative "models/link"
 require_relative "models/tag"
@@ -8,4 +10,3 @@ require_relative "models/user"
 
 DataMapper.finalize
 
-DataMapper.auto_upgrade!
